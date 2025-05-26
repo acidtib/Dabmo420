5d340aef0ee2fe868bb4bb70d7931dd6ab986d87
acidtib <arubinofaux@gmail.com>
2025-05-26 15:08:28 -0600

    Refactor guild creation and deletion logic to use upsert functions for user and guild data, improving database interaction and error handling.

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -6,18 +6,18 @@ export default {
 	async execute(guild: any) {
 		console.log(`Left a guild: ${guild.name}`);
 
-        // Get the guild from the database
-        const guildData = await Guild.findOne({
-            where: {
-                discordId: guild.id,
-            },
-        });
+    // Get the guild from the database
+    const guildData = await Guild.findOne({
+      where: {
+        discordId: guild.id,
+      },
+    });
 
-        // if we have a guild lets deactive it
-        if (guildData) {
-            await guildData.update({
-                active: false,
-            });
-        }
+    // if we have a guild lets deactive it
+    if (guildData) {
+      await guildData.update({
+        active: false,
+      });
+    }
 	},
 };
